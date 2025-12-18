bcc4b38af1976fc23820af71ad43efb6f4c0981c
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2025-12-18 17:53:40 +0000

    second largest number inside array

--- secondLargestNo.js
+++ secondLargestNo.js
@@ -1,15 +1,19 @@
 const arr = [2,1,3,8,4,5];
 
 function findSecondLargestNumber(arr){
+    // if array is empty or has only one element
     if(arr.length<2){
         return null;
     }
+
+    // if array has duplicate items
+
     let first=-Infinity,second=-Infinity;
     for(let i=0;i<arr.length;i++){
         if(arr[i]>first){
             second=first;
             first=arr[i];
-        }else if(arr[i]>second){
+        }else if(arr[i]>second && arr[i]!==first){
             second=arr[i];
         }
     }
