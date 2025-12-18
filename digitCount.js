0f286ddc8290b90a57abef94300b33f102a828d9
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2025-12-18 18:53:14 +0000

    digitt count in a given number

--- digitCount.js
+++ digitCount.js
@@ -1,6 +1,10 @@
 const num = 12345;
 function digitCount(n){
     let count = 0;
+    n=Math.abs(n);
+    if(n==0){
+        return 1;
+    }
     while(n>0){
         let rem = n%10;
         count++;
