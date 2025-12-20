7cfa271c32a8aff4e836c054301947ba1f4737c7
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2025-12-20 11:24:40 +0000

    count unique items in array count duplicate items one time

--- uniqueItems.js
+++ uniqueItems.js
@@ -1,5 +1,5 @@
 const arr=[0,1,2,2,3,4,4,4,5,5,6,7];
-function removeDuplicate(arr){
+function countUniqueItems(arr){
 let k=0;
 for(let i=0;i<arr.length;i++){
     if(arr[i]!=arr[k]){
@@ -8,4 +8,4 @@ for(let i=0;i<arr.length;i++){
     return k+1;
 }
 }
-removeDuplicate(arr);
+countUniqueItems(arr);
