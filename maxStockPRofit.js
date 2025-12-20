6cf6d3fa3d0c7b9a73a79b654fa95d5bf8a313f1
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2025-12-20 15:56:28 +0000

    best time to buy and sell stock for max profit

---
+++ maxStockPRofit.js
@@ -0,0 +1,15 @@
+const prices =[7,1,5,3,6,4];
+function maxProfit(prices){
+    let minPrice=prices[0];
+    let maxProfit=0;
+    for(let i=1;i<prices.length;i++){
+        if(prices[i]<minPrice){
+            minPrice=prices[i];
+        }
+        if(prices[i]-minPrice>maxProfit){
+            maxProfit=prices[i]-minPrice;
+        }
+    }
+    return maxProfit;
+}
+console.log(maxProfit(prices));
