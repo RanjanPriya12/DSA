2392d7ae182ac5861ed34489d5cff15934ab7c85
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2025-12-27 16:55:26 +0000

    add a new node at given index in the linkedList

--- linkedList.js
+++ linkedList.js
@@ -8,7 +8,7 @@ function Node(value){
     this.next=null;
 }
 
-const addNodeAtHead=function(value){
+MyLinkedList.prototype.addNodeAtHead=function(value){
     const newNode = new Node(5);
     newNode.next= this.head;
     this.head=newNode;
@@ -16,7 +16,7 @@ const addNodeAtHead=function(value){
 
 }
 
-const addNewNodeAtTail=function(value){
+MyLinkedList.prototype.addNewNodeAtTail=function(value){
     const newNode=new Node(10);
     if(!this.head){
         this.head=newNode;
@@ -26,5 +26,27 @@ const addNewNodeAtTail=function(value){
             current=current.next;
         }
         current.next=newNode;
+    }
+    this.size++;
+}
+
+MyLinkedList.prototype.addNewNodeAtIndex=function(index,value){
+    if(index<0 || index>this.size){
+        return;
+    }
+    if(index===0){
+        this.addNodeAtHead(value);
+        return;
+    }else{
+        const newNode = new Node(value);
+        let current=this.head,previous=null,count=0;
+        while(count<index){
+            previous=current;
+            current=current.next;
+            count++;
+        }
+        newNode.next=current;
+        previous.next=newNode;
+        this.size++;
     }
 }
