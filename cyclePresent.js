bc5f24356cd92e055fe96d29f4dcd7d83999b1b8
Priya Ranjan <97494807+RanjanPriya12@users.noreply.github.com>
2025-12-28 18:25:55 +0000

    check if a cycle is present in LinkedList or not

--- cyclePresent.js
+++ cyclePresent.js
@@ -69,5 +69,9 @@ MyLinkedList.prototype.isCyclePResent=function(){
     while(fast && fast.next){
         slow=slow.next;
         fast=fast.next.next;
+        if(slow===fast){
+            return true;
+        }
     }
+    return false;
 }
